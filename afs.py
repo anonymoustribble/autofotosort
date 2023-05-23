ca42ca015b402ff118d64590aa5702053a2e3b99
anonymoustribble <tomgar@umich.edu>
2023-05-23 11:11:12 -0700

    organize functions in seperate file

--- afs.py
+++ afs.py
@@ -2,7 +2,7 @@
 AutoFotoSort (AFS)
 Version: 1.5
 Created: Jan 6 2023
-Last Modified: Apr 19 2023
+Last Modified: Apr 21 2023
 (c) 2023 anonymoustribble
 """
 
@@ -11,14 +11,13 @@ import os
 import shutil
 import time
 
+import sorting as sort
+
 #CONSTANTS
 YEAR = "2023"
 VERSION = "1.5"
 DIRECTORY_OVERRIDE = ""
-MONTHS = ["01-Jan", "02-Feb", "03-Mar", "04-Apr", "05-May", "06-Jun", "07-Jul", "08-Aug", "09-Sep", "10-Oct", "11-Nov", "12-Dec"]
-
-#GLOBAL VARIABLES
-
+MONTHS = ("01-Jan", "02-Feb", "03-Mar", "04-Apr", "05-May", "06-Jun", "07-Jul", "08-Aug", "09-Sep", "10-Oct", "11-Nov", "12-Dec") 
 
 #START AND SETUP
 
@@ -34,155 +33,6 @@ def start():
         return DIRECTORY_OVERRIDE
 
 
-#GET IMAGES FROM DIRECTORY
-    
-def getImages(folder):
-
-    print("> locating images...")
-    
-    #initialize variables, get list of everything in folder
-    fileList = []
-    removeList = []
-    try:
-        os.chdir(folder)
-        fileList = os.listdir()
-    except:
-        print("> error x3: unable to locate directory "+folder)
-        exit()
-    
-    #remove folders from fileList
-    for file in fileList:
-        folderFlag = True
-
-        for letter in file:
-
-            if letter == ".":
-                folderFlag = False
-                break
-
-        if folderFlag == True:
-            removeList.append(file)
-
-    for file in removeList:
-        fileList.remove(file)
-  
-    #error checking: empty directory    
-    if fileList == []:
-        print("> error x1: no files detected in this directory")
-        time.sleep(2)
-        exit()
-        
-    #ask for user confirmation to proceed
-    if input("> found "+str(len(fileList))+" photos. Proceed (y/n)? ") != "y":
-        exit()
-
-    return fileList
-    
-    
-
-#CREATE FOLDERS FOR ORGANIZATION BY YEAR
-
-def createFolders(fileList):
-
-    print("> detected years...")
-
-    #initialize variables
-    yearList = []
-    
-    #check through first 4 chars of file, add to yearlist
-    for file in fileList:
-        year = file[0:4]
-
-        if year not in yearList:
-            yearList.append(year)
-            print("> "+year)
-
-    #conduct error checking, get directories that already exist
-    existingYearList = checkFiles(fileList, yearList)
-
-    #make directories that correspond to yearList
-    for year in yearList:
-
-        #if directory already exists, skip it
-        if year not in existingYearList:
-
-            for month in MONTHS:
-                os.makedirs(year+"/"+month)
-
-    print("> folder structure complete")
-    time.sleep(2)
-
-
-#SORT PHOTOS
-
-def sortPhotos(fileList):
-
-    count = 0
-
-    #go through each file and move it
-    for file in fileList:
-
-        count += 1
-
-        #get year and month from filename
-        year = file[0:4]
-        monthNum = file[4:6]
-        month = MONTHS[int(monthNum)-1]
-        
-        #use shutil.move to move dat file!
-        shutil.move(file, year+"/"+month)
-        print("> move "+file)
-        
-    print("> moved "+str(count)+" files")
-
-
-#CHECK FOR ERRORS AND ABORT IF FOUND
-
-def checkFiles(fileList, yearList):
-
-    #initialize variables
-    existingYearList = []
-    flag = False
-    error = ""
-
-    #ensure that first 6 chars of filename are numbers
-    for file in fileList:
-        try:
-            int(file[0:6])
-        except:
-            flag = True
-            error = "x2: invalid file name: " + file
-
-    #get list of already existing year folders
-    for year in yearList:
-        files = os.listdir()
-        if year in files:
-            existingYearList.append(year)
-            print("> found directory "+str(year))
-
-    #ensure that chars 4-6 are a valid month
-    erroneousfilename = ""
-    try:
-        for file in fileList:
-            erroneousfilename = file
-            if int(file[4:6]) > 12:
-                flag = True
-                error = "x4: "+file[4:6]+" is not a valid month: " + file
-    
-    #check if something really weird happens
-    except:
-        flag = True
-        error = "x5: invalid file names" + erroneousfilename
-
-    #if flag was set to true, then there is an error somewhere            
-    if flag == True:
-        print("> error "+error)
-        time.sleep(2)
-        exit()
-
-    return existingYearList
-        
-
 #MAINLOOP
 
 def main():
@@ -195,16 +45,17 @@ def main():
     folder = start()
 
     #Get a list of images in directory
-    fileList = getImages(folder)
+    fileList = sort.getImages(folder)
 
     #Create folder structure
-    createFolders(fileList)
+    sort.createFolders(fileList, MONTHS)
 
     #Sort photos
-    sortPhotos(fileList)
+    sort.sortPhotos(fileList, MONTHS)
     
     print("> operation complete")
     time.sleep(2)
+
 
 main()
     
